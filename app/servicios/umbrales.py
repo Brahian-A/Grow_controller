55a2127ab7083e29082a9ea1ec2340aa3ef48550
Brahia-A <10114@holbertonstudents.com>
2025-11-05 04:04:06 -0300

    error en lso nombres

--- app/servicios/umbrales.py
+++ app/servicios/umbrales.py
@@ -98,10 +98,10 @@ def procesar_umbrales(db: Session, esp_id: str, lectura: Lectura):
 
     # --- 2. HUMEDAD AMBIENTAL (Ventilador) ---
     # Lógica: Si la humedad ambiente es muy alta, forzar encendido del ventilador.
-    if lectura.humedad_ambiente is not None and cfg.humedad_ambiente is not None:
+    if lectura.humedad is not None and cfg.humedad is not None:
         try:
-            hum_amb = float(lectura.humedad_ambiente)
-            set_hum_amb = float(cfg.humedad_ambiente)
+            hum_amb = float(lectura.humedad)
+            set_hum_amb = float(cfg.humedad)
             
             high_hum_amb = set_hum_amb + margen_hum_amb # Umbral superior para encender ventilador
             
